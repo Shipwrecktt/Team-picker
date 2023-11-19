f8a3a87b86c6d8899779a04e16bea18f70bd52cf
Shipwrecktt <141641553+Shipwrecktt@users.noreply.github.com>
2023-11-19 18:03:15 +0000

    Update .py

--- .py
+++ .py
@@ -1,11 +1,11 @@
 import random                                                                   
 
 print("welcome to my team allocator")                                           
-players =["Connor","James","sadam","Osama","Jeb","Al","Comraid","Boris"]        
+players =["Player1","Player2","Player3","Player4","Player5","Player6","Player7","Player8"]        #modify the names to whatever you want ^^
 while True: 
     random.shuffle(players) 
 
-    response = input("is this Team or individual? \nType team or individual ")
+    response = input("is this Team or individual? \nType team or individual \n")
     if response == "team":
 
         team1 = players[:len(players)//2]
@@ -20,18 +20,20 @@ while True:
         for player in team2:
             print(player)
 
-        response = input("if you want to go again press y if not press n" )
+        response = input("if you want to go again press y if not press n ")
         if response == "n":
             break
 
     else:
-        for i in range(0, 20, 2):
+        for i in range(0, 20, 2):                                 
             print(players[i]+ " vs " + players[i+1])
             start = random.randrange(i, i+2)
             print(players[start] + " starts")
 
-            response = input("would you like to go again if so press y if not press n ")
+            response = input("would you like to go again if so press y if not press n ") #program will crash if you continue pressing Y
             if response == "n":
                 break
+
+
 
 
